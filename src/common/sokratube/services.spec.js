bff371559a1c39407ba335489b5b84c00824d790
himangshu <himangshu@sokratik.com>
2014-01-08 07:15:41 +0530

    [v3.0.10] edit mode sharing fixes
    1. removed redundant state abstraction in edit mode
    2. fixed the scope for page in goToPage
    3. fixed all test cases
    
    Signed-off-by: himangshu <himangshu@sokratik.com>

--- src/common/sokratube/services.spec.js
+++ src/common/sokratube/services.spec.js
@@ -2,20 +2,22 @@ describe('sokratube services ', function () {
     beforeEach(function () {
         module("sokratik.atelier.sokratube.services");
     });
-    var divId, options,dummyAudio;
-    beforeEach(inject(function (apollo) {
+    var divId, options, dummyAudio;
+    beforeEach(inject(function (apollo, $state) {
         window.YT = {
             Player: function (_divId, _options) {
                 divId = _divId;
                 options = _options;
-                this.getCurrentTime = function(){
+                this.getCurrentTime = function () {
                     return 10;
                 };
             },
-            PlayerState: {PLAYING:1,END:2}
+            PlayerState: {PLAYING: 1, END: 2}
         };
-        dummyAudio = {volume:1,play:function(){}};
+        dummyAudio = {volume: 1, play: function () {
+        }};
         apollo.addBGAudio(dummyAudio);
+        $state.current = {data: {mode: 'play'}};
         apollo.initBGAudio();
 
     }));
@@ -39,13 +41,13 @@ describe('sokratube services ', function () {
         waitsFor(function () {
             return notified;
         }, "sokratube not resolved", 1000);
-        runs(function(){
+        runs(function () {
             expect(divId).toBe("player");
             expect(dummyAudio.volume).toBe(0);
         });
     }));
     it("modal time out close", inject(function (sokratube, $q, $rootScope) {
-        var promise = sokratube.initYTVideo({videoId: "dummy",timeOut: 50}, $q.defer());
+        var promise = sokratube.initYTVideo({videoId: "dummy", timeOut: 50}, $q.defer());
         expect(dummyAudio.volume).toBe(0.1);
         var scope = $rootScope.$new();
         runs(function () {
@@ -70,10 +72,10 @@ describe('sokratube services ', function () {
         waitsFor(function () {
             return notified;
         }, "sokratube not resolved", 1000);
-        runs(function(){
+        runs(function () {
             expect(divId).toBe("player");
             expect(dummyAudio.volume).toBe(0);
-            options.events.onStateChange({data:YT.PlayerState.PLAYING});
+            options.events.onStateChange({data: YT.PlayerState.PLAYING});
         });
         runs(function () {
             setTimeout(function () {
@@ -84,12 +86,12 @@ describe('sokratube services ', function () {
         waitsFor(function () {
             return resolved;
         }, "sokratube not resolved", 3000);
-        runs(function(){
+        runs(function () {
             expect(dummyAudio.volume).toBe(0.1);
         });
     }));
     it("modal video end close", inject(function (sokratube, $q, $rootScope) {
-        var promise = sokratube.initYTVideo({videoId: "dummy",timeOut: null}, $q.defer());
+        var promise = sokratube.initYTVideo({videoId: "dummy", timeOut: null}, $q.defer());
         expect(dummyAudio.volume).toBe(0.1);
         var scope = $rootScope.$new();
         runs(function () {
@@ -113,10 +115,10 @@ describe('sokratube services ', function () {
         waitsFor(function () {
             return notified;
         }, "sokratube not resolved", 1000);
-        runs(function(){
+        runs(function () {
             expect(divId).toBe("player");
             expect(dummyAudio.volume).toBe(0);
-            options.events.onStateChange({data:YT.PlayerState.END});
+            options.events.onStateChange({data: YT.PlayerState.END});
         });
         runs(function () {
             setTimeout(function () {
@@ -127,7 +129,7 @@ describe('sokratube services ', function () {
         waitsFor(function () {
             return resolved;
         }, "sokratube not resolved", 3000);
-        runs(function(){
+        runs(function () {
             expect(dummyAudio.volume).toBe(0.1);
         });
     }));
