64fe1469be5a806cd83b7e4e64e4d8e64a255511
himangshu <himangshu@sokratik.com>
2014-01-09 13:10:14 +0530

    Revert "[v3.0.14] caching the presentation only in play mode"
    This reverts commit f6dd59cfab53a008a2d33b7f6b2ca3ea8f733054.

--- src/common/istari/services.js
+++ src/common/istari/services.js
@@ -79,7 +79,7 @@
                 post: function (presentation) {
                     //noinspection JSUnresolvedFunction
                     var deferred = $q.defer();
-                    _cache[presentation._id] = null;
+                    _cache[presentation._id] = presentation;
                     presentation.$update(function (resp) {
                         deferred.resolve(presentation);
 
@@ -87,9 +87,9 @@
                     return deferred.promise;
                 },
                 fetchImages: _fetchImages,
-                fetchPresentation: function (presentationId,cache) {
+                fetchPresentation: function (presentationId) {
                     var deferred = $q.defer();
-                    if (_cache[presentationId] && cache) {
+                    if (_cache[presentationId]) {
                         deferred.resolve(_cache[presentationId]);
                     } else {
                         presentation.get({presentationId: presentationId}, function (presentation) {
